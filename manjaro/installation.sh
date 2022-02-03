fa3274c95e822818e7f8d158db7cb7ac277fdba9
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-02-02 20:10:16 -0600

    Adds traceroute and nmap.

--- manjaro/installation.sh
+++ manjaro/installation.sh
@@ -2,7 +2,7 @@ echo ""
 echo "Press ENTER to install all packages through pacman"
 echo "Command: pacman -S steam wine ntfs-3g base-devel git chromium discord lutris gimp openshot neofetch cmatrix bleachbit python-pip audacity npm"
 read
-sudo pacman -S steam wine ntfs-3g base-devel git chromium discord lutris gimp openshot neofetch cmatrix bleachbit python-pip audacity go npm
+sudo pacman -S steam wine ntfs-3g base-devel git chromium discord lutris gimp openshot neofetch cmatrix bleachbit python-pip audacity go npm traceroute
 
 echo ""
 echo "Type in your username. MUST be your username!"
@@ -21,7 +21,7 @@ makepkg -si
 echo ""
 echo "Press ENTER to install all packages with yay"
 echo "Command: yay -S code etcher obs-studio openshot simplenote-electron-bin spotify"
-yay -S code etcher obs-studio openshot simplenote-electron-bin spotify
+yay -S code etcher obs-studio openshot simplenote-electron-bin spotify gnome-books nmap
 
 echo ""
 echo "Generate ssh key."
